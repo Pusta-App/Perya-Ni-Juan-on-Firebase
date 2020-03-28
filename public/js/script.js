19454f69889e1c6cc18ec18e7c1ee1e27e6bb5ad
Bytes Crafter <bytescrafter@gmail.com>
2020-03-28 21:27:07 +0800

    Added new static website for the app.

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+const g3bettem = document.getElementById("g3bettem");
+if(g3bettem != null) {
+    g3bettem.addEventListener("click", (event) => {
+        event.preventDefault();
+        
+    });
+}
